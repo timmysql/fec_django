a1a5bd64786c7098fe952cf15827e6a08379d649
timmysql <timkonecky@outlook.com>
2022-07-13 14:16:24 -0600

    readme.md

--- settings.py
+++ settings.py
@@ -7,3 +7,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 FEC_API_KEY = os.environ.get('fec_api_key')
+HOST = os.environ.get('host')
+DATABASE = os.environ.get('database')
+USER = os.environ.get('user')
+PASSWORD = os.environ.get('password')
